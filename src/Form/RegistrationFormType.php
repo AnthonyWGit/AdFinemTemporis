7066592023ed55c73207fc9494a9127fd4faba3f
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-10-11 11:39:19 +0200

    creating registration form with some constraints

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Player;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+             ->add('username', TextType::class,
+                [
+                    'constraints' => [
+                new NotBlank()]
+                    ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('email', TextType::class)
+            ->add('plainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => [
+                    'attr' => ['class' => 'password-field'],
+                    'row_attr' => ['class' => 'formRow'], //This allows us to have class on our formRow and we don't have to write widget/labels/etc
+                ],
+                'required' => true,
+                'first_options' => ['label' => 'Mot de Passe'],
+                'second_options' => ["label" => 'Entrez le Mot de passe à nouveau'],
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex([
+                        'pattern' => '~^(?=.{12,}$)(?=.*\p{Lu})(?=.*\p{Ll})(?=.*\d)(?=.*[@#$%^&+=!]).*$~',
+                        'message' => 'Ce MdP ne correspond pas aux consignes'
+                    ]),
+                ]
+            ])
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Player::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token', 
+            'csrf_token_id' => 'your_csrf_token_id',
+        ]);
+    }
+}
