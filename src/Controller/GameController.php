4982f7bd87cba87321bc1c9e8846bf05c556c0dd
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-10-17 10:52:55 +0200

    loadind js at the end of body

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class GameController extends AbstractController
 {
+
+    // #[Route('/get-mp3-data', name: 'get_mp3_data')]
+    // public function getMp3Data(): Response
+    // {
+    //     $mp3FilePath = realpath($this->getParameter('kernel.project_dir') . '/public/sfx/typewriter.mp3');
+    //     $data = ['mp3FilePath' => $mp3FilePath];
+    //     return new JsonResponse($data);
+    // }
+
     #[Route('/game', name: 'game')]
     public function index(): Response
     {
