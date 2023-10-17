8f779b918c3a331e926502c4381079bd7ae2424a
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-10-17 17:02:19 +0200

    test if i have a lot of text

--- public/js/stageOne.js
+++ public/js/stageOne.js
@@ -2,22 +2,45 @@ function typeWriter() {
     if (index < text.length) {
         document.querySelector(".TextDiv").innerHTML += text.charAt(index);
         index++;
-        setTimeout(typeWriter, 5); // Delay between each character
+        setTimeout(typeWriter, 1); // Delay between each character
     }
     else
     {   setTimeout(function (){
         isTypingInProgress = false
         index = 0
         document.querySelector(".TextDiv").innerHTML = ""
+        $('.centerTextBox').show();
+        speakerBox.innerHTML = "Horus"
+        textBox.innerHTML = "I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward." +
+        "I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward."
+        +
+        "I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward."
+        +
+        "I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward."
+        +"I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward."
+        +"I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward."
+        +"I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward."
+        +"I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward."
+        +"I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward."
+        +"I didn't think you would be a birdie person. Expect the others to be pretty much mad. We don't have much time, let's press forward."
+
     }, 2000) // Do nothing and wait 3 seconds 
 
     }
 }
 
+//jQuery 
+
+$(document).ready(function() {
+    // Hide the centerTextBox when the document is ready because we want to display X has joined your team ! 
+    $('.centerTextBox').hide();
+
+});
 
 //Vars initialization
 //Texts
-const text = "Horus joined your team !"
+var jsVar = $(".TextDiv").data("var");
+const text = jsVar + " joined your team !"
 
 let index = 0;
 let isTypingInProgress = false;
@@ -29,9 +52,19 @@ var audio = new Audio(mp3FilePath);
 let range = document.querySelector("#volume")
 let rangeValue = document.querySelector("#volume").value
 let ephemeral = document.querySelectorAll(".ephemeral")
+let textBox = document.querySelector(".textBox")
+let speakerBox = document.querySelector(".speakerBox")
+
+let textContent = document.querySelector('.text-content');
+let isScrolling = false;
+
+
+// Function to handle Spacebar key press for scrolling
+
+  
+
 
 //eventListeners
-
 document.querySelector('#mute').addEventListener('click', function() {
     if (audio.muted == false)
     {
@@ -50,4 +83,5 @@ volume.addEventListener('input', function() {
     console.log(rangeValue);
 });
 
-document.addEventListener('keyup', choiceHorus);
+
+typeWriter();
