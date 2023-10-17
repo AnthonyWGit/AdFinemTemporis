5c692161bb9600710d0618ab28adfd48095100fb
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-10-17 10:52:16 +0200

    more text, js script not on template anymore

---
+++ public/js/beginning.js
@@ -0,0 +1,144 @@
+function typeWriter() {
+    if (index < text.length) {
+        document.querySelector(".TextDiv").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 50); // Delay between each character
+    }
+    else
+    {   setTimeout(function (){
+        isTypingInProgress = false
+        index = 0
+        document.querySelector(".TextDiv").innerHTML = ""
+        typeWriter2()
+    }, 2000) // Do nothing and wait 3 seconds 
+
+    }
+}
+
+function typeWriter2() {
+    if (index < text2.length) {
+        document.querySelector(".TextDiv").innerHTML += text2.charAt(index);
+        index++;
+        setTimeout(typeWriter2, 50); // Delay between each character
+    }
+    else
+    {   setTimeout(function (){
+        isTypingInProgress = false
+        index = 0
+        typeWriter3()
+    }, 2000) // Do nothing and wait 3 seconds 
+    
+    }
+}
+
+function typeWriter3() {
+    if (index < text3.length) {
+        document.querySelector("#part2").innerHTML += text3.charAt(index);
+        index++;
+        setTimeout(typeWriter3, 50); // Delay between each character
+    }
+    else
+    {   setTimeout(function (){
+        isTypingInProgress = false
+        index = 0
+        typeWriter4()
+    }, 2000) // Do nothing and wait 3 seconds 
+    
+    }
+}
+
+function typeWriter4() {
+    if (index < text4.length) {
+        document.querySelector("#part3").innerHTML += text4.charAt(index);
+        index++;
+        setTimeout(typeWriter4, 50); // Delay between each character
+    }
+    else
+    {   setTimeout(function (){
+        isTypingInProgress = false
+        index = 0
+        typeWriter5()
+    }, 2000) // Do nothing and wait 3 seconds 
+    
+    }
+}
+
+function typeWriter5() {
+    if (index < text5.length) {
+        document.querySelector("#part4").innerHTML += text5.charAt(index);
+        index++;
+        setTimeout(typeWriter5, 50); // Delay between each character
+    }
+    else
+    {   setTimeout(function (){
+        isTypingInProgress = false
+        audio.pause()
+        audio.currentTime = 0; //stopping audio
+        document.querySelector("#part2").appendChild(createButton)
+        document.querySelector("#part3").appendChild(createButton)
+        document.querySelector("#part4").appendChild(createButton)
+
+        
+    }, 2000) // Do nothing and wait 3 seconds 
+    
+    }
+}
+
+
+//Vars initialization
+const text = "Those dreams never felt so real. Night became as deep as ink. Something is happening. Somewhere, somehow." + 
+"How will you shape your fate ? ";
+const text2 = "A long corridor. Endless, wrapped in shadows. In the distance, three figures. To which one are you drawn to ?"
+const text3 = "The first one is a radiant eagle standing on two legs. His gaze is lost into the horizon."
+const text4 = "The second is a floting serpent. Ethereal, hanging in the air; suspended by time."
+const text5 = "The third is barely visible. It is shrouded in darkness, blending in with its surroundings."
+let index = 0;
+let range = document.querySelector("#volume")
+let rangeValue = document.querySelector("#volume").value
+let ephemeral = document.querySelectorAll(".ephemeral")
+let isTypingInProgress = false;
+
+
+
+const createButton = document.createElement("button")
+createButton.textContent = "Select"
+const mp3FilePath = '/sfx/typewriter.mp3';
+var audio = new Audio(mp3FilePath);
+
+document.getElementById('yesButton').addEventListener('click', function() {
+    audio.muted = false; // Unmute the audio
+    audio.volume = 0.3;
+    audio.play();
+    ephemeral.forEach(function (element) {
+        element.remove()
+    }); 
+    typeWriter();
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    audio.pause(); // Pause the audio
+    audio.currentTime = 0; // Reset audio to the beginning
+    ephemeral.forEach(function (element) {
+        element.remove()
+    }); 
+    typeWriter();
+});
+
+document.querySelector('#mute').addEventListener('click', function() {
+    if (audio.muted == false)
+    {
+        audio.muted = true; // Unmute the audio
+        audio.play();
+    }
+    else
+    {
+        audio.muted = false; // Unmute the audio            
+    }
+});
+
+volume.addEventListener('input', function() {
+    rangeValue = document.querySelector("#volume").value
+    audio.volume = (rangeValue / 100)
+    console.log(rangeValue);
+});
+
