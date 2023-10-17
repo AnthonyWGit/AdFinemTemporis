b8ac0c4ca00be03392a6e9ace29a140da1db19d5
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-10-17 14:47:59 +0200

    controller for choosing demon

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\DemonBase;
+use App\Entity\DemonTrait;
+use App\Entity\DemonPlayer;
+use App\Repository\DemonBaseRepository;
+use App\Repository\DemonTraitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -27,9 +34,55 @@ class GameController extends AbstractController
                 'controller_name' => 'GameController',
             ]);           
         }
+        else if ($this->getUser()->getStage() == 1)
+        {
+            return $this->render('game/stageOne.html.twig', [
+                'controller_name' => 'GameController',
+            ]);    
+        }
         else
         {
 
         }
+    }
+
+    #[Route('/game/choice/{name}', name: 'choice', requirements : ['name' =>  '\w+'])]
+    public function choiceHorus(string $name, DemonBaseRepository $demonBaseRepository, DemonTraitRepository $demonTraitRepository, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->getUser()->getStage() == 0 && $name == 'Horus')
+        {
+            $horus = $this->pickDemonBase($demonBaseRepository, 'horus');
+            $horusTrait = $this->traitGen($demonTraitRepository);
+            $demonPlayer = new DemonPlayer; //create a demon
+            $demonPlayer->setDemonBase($horus); //set base template
+            $demonPlayer->setTrait($horusTrait); //generate a trait
+            $this->getUser()->addDemonPlayer($demonPlayer);
+            $this->getUser()->setStage(1);
+            $entityManager->persist($demonPlayer);
+            $entityManager->flush();
+            return $this->redirectToRoute("game");
+        }   
+        else
+        {
+            return $this->redirectToRoute('app_home');
+        }
+    }
+
+    public function traitGen(DemonTraitRepository $demonTraitRepository) : DemonTrait
+    {
+        $traits = $demonTraitRepository->findBy([], ["name" => "ASC"]); 
+        $randomSetTraits = [];
+        foreach ($traits as $trait)
+        {
+            $randomSetTraits[] = $trait;
+        }
+        $pickedTrait = array_rand($randomSetTraits);
+        return $randomSetTraits[$pickedTrait];
+    }
+
+    public function pickDemonBase(DemonBaseRepository $demonBaseRepository, string $demonName) : DemonBase
+    {
+        $horus = $demonBaseRepository->findOneBy(["name" => "Horus"]);
+        return $horus;
     }
 }
