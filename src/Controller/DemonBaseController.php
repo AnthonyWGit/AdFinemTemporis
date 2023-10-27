cd73f84172de15a5c1f202cb5775a67e9bfd7ac6
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-10-27 09:07:00 +0200

    formType for new/edit demonBase

--- src/Controller/DemonBaseController.php
+++ src/Controller/DemonBaseController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\DemonBase;
+use App\Form\DemonBaseFormType;
 use App\Repository\DemonBaseRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,5 +28,33 @@ class DemonBaseController extends AbstractController
         return $this->render('demon_base/detail.html.twig', [
             'demon' => $demon,
         ]);
+    }
+
+    #[Route('demon/base/new', name: 'newDemonBase')]
+    #[Route('demon/base/{id}/edit', name: 'editDemonBase')]
+    public function new(DemonBase $demonBase = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // creates a task object and initializes some data for this example
+        if ($demonBase === null) {
+            $demonBase = new DemonBase();
+        }
+        $form = $this->createForm(DemonBaseFormType::class, $demonBase);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) 
+            {
+                $demonBase = $form->getData();
+                $entityManager->persist($demonBase); //traditional prepare / execute in SQL MANDATORY for sql equivalents to INSERT 
+                $entityManager->flush();
+
+                $this->addFlash // need to be logged as user to see the flash messages build-in Symfony
+                (
+                    'notice',
+                    'Your changes were saved!'
+                );
+
+                return $this->redirectToRoute('demonsList'); //redirect to list stagiaires if everything is ok
+            }
+        
+        return $this->render("demon_base/new.html.twig", ['formNewDemonBase' => $form, 'edit' => $demonBase->getId()]);
     }
 }
