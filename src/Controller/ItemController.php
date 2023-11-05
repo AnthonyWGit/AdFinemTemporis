3bc04d59129a778e687c0e194593d4321a2c194b
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-11-05 14:24:31 +0100

    preparing controller for items

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Item;
+use App\Form\ItemFormType;
 use App\Repository\ItemRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,6 +20,54 @@ class ItemController extends AbstractController
         return $this->render('item/index.html.twig', [
             'items' => $items,
         ]);
+    }
+
+    #[Route('/item/show/{name}', name: 'itemDetail')]
+    public function detail(Item $item): Response
+    {
+        return $this->render('item/detail.html.twig', [
+            'item' => $item,
+        ]);
+    }
+
+    #[Route('admin/skill/new', name: 'newSkill')]
+    #[Route('admin/skill/{name}/edit', name: 'editSkill')]
+    public function new(Item $item = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // creates a task object and initializes some data for this example
+        if ($item === null) {
+            $item = new Item();
+        }
+        $form = $this->createForm(ItemFormType::class, $Item);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) 
+            {
+                $item = $form->getData();
+                $entityManager->persist($skill); //traditional prepare / execute in SQL MANDATORY for sql equivalents to INSERT 
+                $entityManager->flush();
+
+                $this->addFlash // need to be logged as user to see the flash messages build-in Symfony
+                (
+                    'noticeChange',
+                    'Your changes were saved!'
+                );
+
+                return $this->redirectToRoute('itemsList'); //redirect to list stagiaires if everything is ok
+            }
+        
+        return $this->render("item/new.html.twig", ['formNewItem' => $form, 'edit' => $item->getId()]);
+    }
+
+    #[Route('admin/item/{name}/delete', name: 'deleteItem')]
+    public function itemDelete(Item $item, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($item);
+        $entityManager->flush();
+        $this->addFlash(
+            'noticeChange',
+            'This entry has been deleted'
+        );
+        return $this->redirectToRoute('itemsList');
     }
 
 }
