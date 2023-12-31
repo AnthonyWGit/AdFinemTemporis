6f55cb5a9a85656c29eec1c2c27e6d290660688d
AnthonyWGit <anthony.wetzstein@gmail.com>
2023-12-31 15:50:37 +0100

    changing the access denied exception to redirectioon to account page

---
+++ src/EventListener/AccessDeniedListener.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class AccessDeniedListener implements EventSubscriberInterface
+{
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => ['onKernelException', 2],
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        if (!$exception instanceof AccessDeniedException) {
+            return;
+        }
+
+        $url = $this->urlGenerator->generate('account');
+
+        $response = new RedirectResponse($url);
+
+        $event->setResponse($response);
+    }
+}
