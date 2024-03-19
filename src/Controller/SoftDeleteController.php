7b3e7d0a6bc3500aeadda97d77eca1764f95f5ed
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-03-19 11:10:36 +0100

    using slug

--- src/Controller/SoftDeleteController.php
+++ src/Controller/SoftDeleteController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SoftDeleteController extends AbstractController
@@ -12,9 +13,12 @@ class SoftDeleteController extends AbstractController
     #[Route('/account/softdelete/confirm', name: 'app_soft_delete')]
     public function index(EntityManagerInterface $em): Response
     {
+        $slugger = new AsciiSlugger();
+        $newSlug = $slugger->slug("Deleted User")."-".bin2hex(random_bytes(5));
         $today = new \DateTime();
         // $filter = $em->getFilters()->enable('soft-deleteable'); 
         // $filter->enableForEntity('Entity\Player');
+        $this->getUser()->setSlug($newSlug);
         $this->getUser()->setDeletedAt($today);
         $this->getUser()->setUsername(null);
         $this->getUser()->setEmail(null);
